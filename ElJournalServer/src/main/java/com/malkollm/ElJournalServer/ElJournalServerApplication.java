55eebfd6206c6726ee6490c25e52bb2873bcde74
Malkollm <malkollm.mob.projects@gmail.com>
2025-01-10 20:59:38 +0500

    Initial commit

---
+++ ElJournalServer/src/main/java/com/malkollm/ElJournalServer/ElJournalServerApplication.java
@@ -0,0 +1,13 @@
+package com.malkollm.ElJournalServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElJournalServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElJournalServerApplication.class, args);
+	}
+
+}
---
+++ ElJournalServer/src/test/java/com/malkollm/ElJournalServer/ElJournalServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malkollm.ElJournalServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElJournalServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
